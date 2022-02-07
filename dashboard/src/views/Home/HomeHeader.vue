7649ec1ad527c13b9f078945217c1e9e6fbd50cf
machadomatt <matheusmacsyn@gmail.com>
2022-02-06 22:49:53 -0300

    Apply Home Layout

---
+++ dashboard/src/views/Home/HomeHeader.vue
@@ -0,0 +1,57 @@
+<template>
+    <header class="h-[700px] bg-brand-main w-full flex justify-center">
+        <div
+            class="flex flex-col w-4/5 max-w-6xl sm:bg-[url('../../assets/images/blue-balloons.png')] bg-[length:628px] bg-[90%_bottom] bg-no-repeat"
+        >
+            <div class="flex items-center justify-between py-10">
+                <div class="w-28 lg:w-36">
+                    <img
+                        class="w-full"
+                        src="../../assets/images/logo-white.png"
+                        alt="Feedbacker"
+                    />
+                </div>
+                <div class="flex">
+                    <button
+                        class="px-3 py-2 text-xs font-bold text-white rounded-full sm:text-base sm:px-6 focus:outline-none"
+                    >
+                        Crie uma conta
+                    </button>
+                    <button
+                        class="px-3 py-2 text-xs font-bold bg-white rounded-full sm:text-base sm:px-6 text-brand-main focus:outline-none"
+                    >
+                        Entrar
+                    </button>
+                </div>
+            </div>
+            <div class="flex flex-col mt-28">
+                <h1 class="mb-3 text-4xl font-black text-white">
+                    Tenha um feedback. <br />
+                    E faça seus clientes mais
+                    <br class="hidden lg:inline-block" />
+                    felizes!
+                </h1>
+                <p class="mb-6 text-lg font-normal text-white">
+                    Receba ideias, reclamações e feedbacks com um
+                    <br class="hidden lg:inline-block" />simples widget na
+                    página.
+                </p>
+                <div>
+                    <button
+                        class="px-6 py-2 font-bold bg-white rounded-full text-brand-main focus:outline-none"
+                    >
+                        Crie uma conta grátis
+                    </button>
+                </div>
+            </div>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    setup() {
+        return {}
+    }
+}
+</script>
--- dashboard/src/views/Home/index.vue
+++ dashboard/src/views/Home/index.vue
@@ -1,5 +1,38 @@
 <template>
-    <h1>Home</h1>
+    <HomeHeader @create-account="handleCreateAccount" @login="handleLogin" />
+    <HomeContact />
+    <div class="flex justify-center py-10 bg-brand-gray">
+        <p class="font-medium text-center text-gray-800">
+            Feedbacker &copy; 2022
+        </p>
+    </div>
 </template>
 
-<script></script>
+<script>
+import HomeHeader from './HomeHeader.vue'
+import HomeContact from './HomeContact.vue'
+import { onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+
+export default {
+    components: {
+        HomeHeader,
+        HomeContact
+    },
+    setup() {
+        const router = useRouter()
+
+        onMounted(() => {
+            const token = window.localStorage.getItem('token')
+            if (token) {
+                router.push({ name: 'feedbacks' })
+            }
+        })
+
+        const handleLogin = () => {}
+        const handleCreateAccount = () => {}
+
+        return { handleLogin, handleCreateAccount }
+    }
+}
+</script>
