e00eecfdebbc02f238a11906933b0458c1ef676a
machadomatt <matheusmacsyn@gmail.com>
2022-02-21 00:15:04 -0300

    Manage routes with logged in user

--- dashboard/src/App.vue
+++ dashboard/src/App.vue
@@ -4,11 +4,35 @@
 </template>
 
 <script>
+import { watch } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+import services from '@/services'
 import ModalFactory from './components/ModalFactory'
 
 export default {
     components: {
         ModalFactory
+    },
+    setup() {
+        const router = useRouter()
+        const route = useRoute()
+
+        watch(
+            () => route.path,
+            async () => {
+                if (route.meta.hasAuth) {
+                    const token = window.localStorage.getItem('token')
+
+                    if (!token) {
+                        router.push({ name: 'home' })
+                        return
+                    }
+
+                    const { data } = await services.users.getMe()
+                    console.log(data)
+                }
+            }
+        )
     }
 }
 </script>
